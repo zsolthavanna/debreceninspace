9d549a422e9722d4e04a9a7f3bdabdd390c00818
Nikolai Chakhvashvili <igr1s@mailbox.unideb.hu>
2024-11-16 17:05:49 +0100

    some changes

--- script/enemy.gd
+++ script/enemy.gd
@@ -8,9 +8,15 @@ var direction = 1
 @onready var ray_cast_left = $RayCastLeft
 
 func _process(delta):
-	
 	if ray_cast_right.is_colliding():
 		direction = -1
 	if ray_cast_left.is_colliding():
 		direction = 1
 	position.x += direction * SPEED * delta
+
+# Function to handle collision with the player
+func _on_collision_with_player(player):
+	if player.velocity.y < 0:  # If player is moving upwards (jumping)
+		queue_free()  # Enemy is defeated
+	else:
+		player._on_player_died()  # Call player's death function
