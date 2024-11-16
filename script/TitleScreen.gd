8d6c110980f05f912a7ff240d5897f19324de1f9
Zheenbekov Temirlan <temirlan.2004@mailbox.unideb.hu>
2024-11-16 17:28:27 +0100

    main menu

--- script/TitleScreen.gd
+++ script/TitleScreen.gd
@@ -13,3 +13,13 @@ func _on_start_pressed() -> void:
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 	pass 
+
+
+func _on_start_mouse_entered() -> void:
+	$ClickStart.play()
+	pass # Replace with function body.
+
+
+func _on_exit_mouse_entered() -> void:
+	$ClickExit.play()
+	pass # Replace with function body.
